3fff7c2a7fc58ecb9a60bd3e906f7810da1c297b
KIMGEUNDU <vlkoklv@naver.com>
2023-12-28 14:15:29 +0900

    [🦾] main 페이지 보이게 하는 기능 추가
    ref: #1

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,36 @@
-import Intro from '@/components/Intro';
-import { Helmet } from 'react-helmet-async';
+import ContactButton from '@/components/ContactButton';
+import Intro from '@/components/Intro/Intro';
+import IntroMenu from '@/components/Intro/IntroMenu';
+import Promise from '@/components/Main/Promise';
+import { useEffect, useState } from 'react';
+import styled from 'styled-components';
 
 // axios.defaults.baseURL = 'https://localhost/api';
 
 export default function Home() {
+  const [endState, setEndState] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setEndState(true);
+    }, 6500);
+  }, []);
+
   return (
     <>
-      <Helmet>
-        <title>Portfolio</title>
-      </Helmet>
-      <Intro />
+      <header>
+        <Intro />
+      </header>
+      <Main isProgressBarEnd={endState}>
+        <IntroMenu />
+        <Promise />
+        <ContactButton />
+      </Main>
     </>
   );
 }
+
+const Main = styled.main<{ isProgressBarEnd: boolean }>`
+  transition: display 2s ease-in-out;
+  display: ${(props) => (props.isProgressBarEnd ? 'block' : 'none')};
+`;
