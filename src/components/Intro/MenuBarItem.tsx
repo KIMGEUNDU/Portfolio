e4208e7086d3dca7cafc37d34251854e3aff40cf
KIMGEUNDU <vlkoklv@naver.com>
2023-12-28 14:23:04 +0900

    [🎃] 인트로메뉴 반응형 작업
    ref: #1

---
+++ src/components/Intro/MenuBarItem.tsx
@@ -0,0 +1,72 @@
+import { useState } from 'react';
+import styled, { keyframes } from 'styled-components';
+
+function MenuBarItem({
+  link,
+  img,
+  delayTime = 1,
+  children,
+}: {
+  link: string;
+  img: string;
+  delayTime?: number;
+  children: string;
+}) {
+  const [endAnimation, setEndAnimation] = useState(false);
+
+  return (
+    <ItemLink id="navLink" href={link}>
+      <Image
+        src={img}
+        alt="Designed by Freepik"
+        $delayTime={delayTime}
+        $endAnimation={endAnimation}
+        onAnimationEnd={() => setEndAnimation(true)}
+      />
+      <ImageTitle>{children}</ImageTitle>
+    </ItemLink>
+  );
+}
+
+const appearImage = keyframes`
+    0%{opacity: 0; transform: scale(0)}
+    100%{opacity: 1; transform: scale(1)}
+`;
+
+const ItemLink = styled.a`
+  position: relative;
+  text-align: center;
+`;
+
+const Image = styled.img<{ $delayTime: number; $endAnimation: boolean }>`
+  opacity: ${(props) => (props.$endAnimation ? '1' : '0')};
+  max-width: 80%;
+  animation: ${appearImage} 2s linear ${(props) => props.$delayTime}s forwards;
+  @media ${({ theme }) => theme.device.mobile} {
+    width: 30%;
+  }
+  @media ${({ theme }) => theme.device.tablet} {
+    width: 40%;
+  }
+`;
+
+const ImageTitle = styled.span`
+  font-size: 25px;
+  opacity: 0;
+  position: absolute;
+  top: 50%;
+  right: 50%;
+  transform: translateX(50%) rotate(-30deg);
+  background-color: black;
+  color: white;
+  padding: 10px 10%;
+  #navLink:hover & {
+    transition: opacity 0.5s;
+    opacity: 1;
+  }
+  @media screen and (max-width: 500px) {
+    font-size: 15px;
+  }
+`;
+
+export default MenuBarItem;
