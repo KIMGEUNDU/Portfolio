de02586f287febc684ec28e68929bb5bbdde7366
KIMGEUNDU <vlkoklv@naver.com>
2023-12-28 17:38:36 +0900

    [📝] Main 생성 후 컴포넌트 정리

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,36 +1,15 @@
-import ContactButton from '@/components/ContactButton';
 import Intro from '@/components/Intro/Intro';
-import IntroMenu from '@/components/Intro/IntroMenu';
-import Promise from '@/components/Main/Promise';
-import { useEffect, useState } from 'react';
-import styled from 'styled-components';
+import Main from '@/layout/Main';
 
 // axios.defaults.baseURL = 'https://localhost/api';
 
 export default function Home() {
-  const [endState, setEndState] = useState(false);
-
-  useEffect(() => {
-    setTimeout(() => {
-      setEndState(true);
-    }, 6500);
-  }, []);
-
   return (
     <>
       <header>
         <Intro />
       </header>
-      <Main isProgressBarEnd={endState}>
-        <IntroMenu />
-        <Promise />
-        <ContactButton />
-      </Main>
+      <Main />
     </>
   );
 }
-
-const Main = styled.main<{ isProgressBarEnd: boolean }>`
-  transition: display 2s ease-in-out;
-  display: ${(props) => (props.isProgressBarEnd ? 'block' : 'none')};
-`;
