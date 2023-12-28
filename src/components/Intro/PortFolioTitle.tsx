8ff2d3499ab0d02c8de93236b8520b9880b00b1a
KIMGEUNDU <vlkoklv@naver.com>
2023-12-28 14:21:56 +0900

    [📦] 포트폴리오 타이틀 완성
    ref: #1

---
+++ src/components/Intro/PortFolioTitle.tsx
@@ -0,0 +1,45 @@
+import styled, { keyframes } from 'styled-components';
+
+function PortFolioTitle() {
+  const title = 'GEONJU&PORTFOLIO';
+
+  return (
+    <Wrapper>
+      {[...title].map((letter, i) => (
+        <LetterItem
+          key={i}
+          style={{
+            animationDelay: `${3 + i * 0.1}s`,
+            color: `${i === 6 ? 'transparent' : ''}`,
+          }}
+        >
+          {letter}
+        </LetterItem>
+      ))}
+    </Wrapper>
+  );
+}
+// 애니메이션
+const appearLetter = keyframes`
+    0% { transform: translateY(100%); }
+  90% { opacity: 1; transform: translateY(0); }
+  100%{ transform: translateY(100%); }
+`;
+
+const Wrapper = styled.div`
+  overflow: hidden;
+  margin: 40px 0;
+  height: 70px;
+  font-size: 60px;
+  @media screen and (max-width: 500px) {
+    font-size: 30px;
+  }
+`;
+
+const LetterItem = styled.span`
+  opacity: 0;
+  display: inline-block;
+  animation: ${appearLetter} 2s forwards;
+`;
+
+export default PortFolioTitle;
