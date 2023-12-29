7ff8fcc78367a89452271f1c049b3fa80e5e497d
KIMGEUNDU <vlkoklv@naver.com>
2023-12-29 18:05:51 +0900

    [📦] IThink 작업중
    ref: #12

--- src/pages/IThink.tsx
+++ src/pages/IThink.tsx
@@ -1,13 +1,30 @@
 import styled from 'styled-components';
+import TypeIt from 'typeit-react';
 
 function IThink() {
-  return <Wrapper id="IThink">IThink</Wrapper>;
+  return (
+    <Wrapper id="IThink">
+      <TypeIt
+        options={{
+          strings: ['I Think'],
+          speed: 100,
+          waitUntilVisible: true,
+          keyboard: true,
+          lifeLike: true,
+        }}
+      />
+    </Wrapper>
+  );
 }
 
 const Wrapper = styled.div`
   width: 100vw;
   height: 100vh;
   background-color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 20vw;
 `;
 
 export default IThink;
