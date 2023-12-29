cab60cf823c46bf01605488568b760ecd3b97e9e
KIMGEUNDU <vlkoklv@naver.com>
2023-12-29 15:15:18 +0900

    [🖼️] I Think 페이지 작업중

---
+++ src/pages/IThink.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+function IThink() {
+  return <Wrapper id="IThink">IThink</Wrapper>;
+}
+
+const Wrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-color: white;
+`;
+
+export default IThink;
