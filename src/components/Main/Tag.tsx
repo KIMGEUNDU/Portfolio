5291b21f1fc25a5d1ae3701c3b8ef33d5e7269c4
KIMGEUNDU <vlkoklv@naver.com>
2024-01-02 02:23:20 +0900

    [📝] 색깔 수정 및 반응형 수정
    ref: #25

--- src/components/Main/Tag.tsx
+++ src/components/Main/Tag.tsx
@@ -2,26 +2,19 @@ import styled from 'styled-components';
 
 function Tag({ value }: { value: string[] }) {
   const color = [
-    '#FCB9AA',
-    '#FFDBCC',
-    '#ECEAE4',
-    '#A2E1DB',
-    '#55CBCD',
-    '#FFAEA5',
-    '#ABDEE6',
-    '#CBAACB',
-    '#FFFFB5',
-    '#FFCCB6',
-    '#F3B0C3',
-    '#C6DBDA',
-    '#FEE1E8',
-    '#FED7C3',
-    '#F6EAC2',
-    '#e7c7f6',
+    'linear-gradient(to top, #a18cd1 0%, #fbc2eb 100%)',
+    'linear-gradient(to right, #ffecd2 0%, #fcb69f 100%)',
+    'linear-gradient(to top, #fdcbf1 0%, #fdcbf1 1%, #e6dee9 100%)',
+    'linear-gradient(to top, #a8edea 0%, #fed6e3 100%)',
+    'linear-gradient(to top, #fff1eb 0%, #ace0f9 100%)',
+    'linear-gradient(to top, #feada6 0%, #f5efef 100%)',
+    'linear-gradient(to top, #accbee 0%, #e7f0fd 100%)',
+    'linear-gradient(to top, #c1dfc4 0%, #deecdd 100%)',
+    'linear-gradient(60deg, #abecd6 0%, #fbed96 100%)',
   ];
 
   const randomColor = () => {
-    const random = Math.round(Math.random() * color.length);
+    const random = Math.round(Math.random() * (color.length - 1));
 
     return color[random];
   };
@@ -37,10 +30,19 @@ function Tag({ value }: { value: string[] }) {
 
 const TagItem = styled.span<{ $color: string }>`
   padding: 1% 2%;
-  font-size: 1.2vw;
+  font-size: 1vw;
   border-radius: 20px;
-  background-color: ${(props) => props.$color};
+  background-image: ${(props) => props.$color};
   white-space: nowrap;
+  @media ${({ theme }) => theme.device.mobile} {
+    font-size: 3vw;
+  }
+  @media ${({ theme }) => theme.device.tablet} {
+    font-size: 2.5vw;
+  }
+  @media ${({ theme }) => theme.device.laptop} {
+    font-size: 1.5vw;
+  }
 `;
 
 export default Tag;
